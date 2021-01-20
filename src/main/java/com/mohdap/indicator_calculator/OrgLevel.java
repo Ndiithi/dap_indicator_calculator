840b96da7c1245854cdd35009a8b9c3b3fa69c0d
Duncan Ndiithi <duncanndiithi@yahoo.com>
2021-01-20 13:36:53 +0300

    orgunits enum

---
+++ src/main/java/com/mohdap/indicator_calculator/OrgLevel.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mohdap.indicator_calculator;
+
+/**
+ *
+ * @author duncanndiithi
+ */
+public enum OrgLevel {
+    LEVEL_1,
+    LEVEL_2,
+    LEVEL_3,
+    LEVEL_4,
+    LEVEL_5
+}
