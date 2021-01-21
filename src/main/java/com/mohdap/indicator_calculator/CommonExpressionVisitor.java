ecaa239cb61da6f5d06d4be9a9a25558a10e3874
Duncan Ndiithi <duncanndiithi@yahoo.com>
2021-01-21 15:23:39 +0300

    calculations dependency

--- src/main/java/com/mohdap/indicator_calculator/CommonExpressionVisitor.java
+++ src/main/java/com/mohdap/indicator_calculator/CommonExpressionVisitor.java
@@ -1,23 +1,8 @@
 package com.mohdap.indicator_calculator;
 
-import com.healthit.indicator_calculator.util.DatabaseSource;
+import com.mohdap.indicator_calculator.service.Aggregator;
 import java.math.BigDecimal;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import org.hisp.dhis.antlr.AntlrExpressionVisitor;
-import org.hisp.dhis.antlr.ParserExceptionWithoutContext;
-
-import java.util.Date;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.stream.Collectors;
 import static org.hisp.dhis.antlr.AntlrParserUtils.castDouble;
 import org.hisp.dhis.parser.expression.antlr.ExpressionParser;
 
@@ -33,21 +18,28 @@ public class CommonExpressionVisitor extends AntlrExpressionVisitor {
 
     final static org.apache.log4j.Logger log
             = org.apache.log4j.Logger.getLogger(CommonExpressionVisitor.class.getCanonicalName());
-    
-    String catDataElementSubSql =" where de.uid='@elementid'";
-    String catOptionComboSubSql = "inner join categoryoptioncombo comb on comb.categoryoptioncomboid  = dt.categoryoptioncomboid \n" +
-                        "where de.uid='@elementid' and comb.uid ='@catoptcomboid'";
-    
-    protected CommonExpressionVisitor() {
+
+    private Period period;
+    private OrgUnit orgunit;
+
+    protected CommonExpressionVisitor(Period period, OrgUnit orgunit) {
+        this.period = period;
+        this.orgunit = orgunit;
     }
 
     @Override
     public Object visitExpr(ExprContext ctx) {
 
         if (ctx.it != null) {
+            log.info("Resolve expression");
             if (ctx.it.getType() == PAREN) { //parenthesis
                 Object value = visit(ctx.expr(0));
-                System.out.println("the value is: PAREN: " + value);
+
+                try {
+                    return (double) value;
+                } catch (ClassCastException ex) {
+                    return (Integer) value;
+                }
             }
 
             if (ctx.it.getType() == MINUS) { //minus
@@ -55,58 +47,118 @@ public class CommonExpressionVisitor extends AntlrExpressionVisitor {
                 Object left = visit(ctx.expr(0));  // get value of left subexpression
                 Object right = visit(ctx.expr(1)); // get value of right subexpression
                 System.out.println("Minus " + left.toString() + " and " + right.toString());
-//            if((String)left ==){
-//                
-//            }
-//            
-//            if ( ctx.op.getType() == LabeledExprParser.ADD ) return left + right;
-//            return left - right; // must be SUB
+                try {
+                    return (double) left - (double) right;
+                } catch (ClassCastException ex) {
+                    return (Integer) left - (Integer) right;
+                }
+
+            }
+
+            if (ctx.it.getType() == PLUS) { //plus
+                System.out.println("Plus " + ctx.expr(0).getText() + " and " + ctx.expr(1).getText());
+                Object left = visit(ctx.expr(0));  // get value of left subexpression
+                Object right = visit(ctx.expr(1)); // get value of right subexpression
+                //System.out.println("Plus " + left.toString() + " and " + right.toString());
+
+                try {
+                    return (double) left + (double) right;
+                } catch (ClassCastException ex) {
+                    return (Integer) left + (Integer) right;
+                }
+
+            }
+
+            if (ctx.it.getType() == POWER) { //power
+
+                Object left = visit(ctx.expr(0));  // get value of left subexpression
+                Object right = visit(ctx.expr(1)); // get value of right subexpression
+                System.out.println("Power " + left.toString() + " and " + right.toString());
+
+                try {
+                    return Math.pow((double) left, (double) right);
+                } catch (ClassCastException ex) {
+                    return Math.pow((Integer) left, (Integer) right);
+                }
+
+            }
+
+            if (ctx.it.getType() == DIV) { //division
+
+                Object left = visit(ctx.expr(0));  // get value of left subexpression
+                Object right = visit(ctx.expr(1)); // get value of right subexpression
+                System.out.println("division " + left.toString() + " and " + right.toString());
+
+                try {
+                    return (double) left / (double) right;
+                } catch (ClassCastException ex) {
+                    return (Integer) left / (Integer) right;
+                }
+            }
+
+            if (ctx.it.getType() == MOD) { //modulus
+
+                Object left = visit(ctx.expr(0));  // get value of left subexpression
+                Object right = visit(ctx.expr(1)); // get value of right subexpression
+                System.out.println("modulus " + left.toString() + " and " + right.toString());
+
+                try {
+                    return (double) left % (double) right;
+                } catch (ClassCastException ex) {
+                    return (Integer) left % (Integer) right;
+                }
+
             }
 
             if (ctx.it.getType() == MUL) { //MULtiplication
 
                 Object left = visit(ctx.expr(0));  // get value of left subexpression
                 Object right = visit(ctx.expr(1)); // get value of right subexpression
-                System.out.println("Multiplying " + left.toString() + " and " + right.toString());
+                System.out.println("MULtiplication " + left.toString() + " and " + right.toString());
+
+                try {
+                    return (double) left * (double) right;
+                } catch (ClassCastException ex) {
+                    return (Integer) left * (Integer) right;
+                }
+
+            }
+
+            if (ctx.it.getType() == NUMERIC_LITERAL) {
+                System.out.println("Numeric literal");
+                System.out.println(ctx.getText());
+
                 return ctx.getText();
+
             }
 
             if (ctx.it.getType() == HASH_BRACE) {
                 System.out.println("resolving hash brace");
                 System.out.println(ctx.uid0.getText());
-                if (ctx.uid1 != null) {
-                    System.out.println(ctx.uid1.getText());
-                }
-                Connection dbCon;
-                try {
-                    dbCon = DatabaseSource.getConnection();
-                    String sql = "Select dt.value from datavalue dt \n"
-                            + "inner join dataelement de on dt.dataelementid=de.dataelementid ";  
-                    if(ctx.uid1!=null){
-                    String appendSubSql=catOptionComboSubSql.replaceAll("@elementid", ctx.uid0.getText()).replaceAll("@catoptcomboid", ctx.uid1.getText());
-                    sql=sql+appendSubSql;
-                    }else{
-                        String appendSubSql=catDataElementSubSql.replaceAll("@elementid", ctx.uid0.getText());
-                        sql=sql+appendSubSql;
-                    }
-                    PreparedStatement st = dbCon.prepareStatement(sql);
-                    ResultSet rs = st.executeQuery();
-                    System.out.println("Got a number: testing");
-                    while (rs.next()) {
-                        System.out.println("Got a number: " + rs.getString(1));
-                    }
 
-                } catch (SQLException ex) {
-                    log.error(ex);
+                String comboId = null;
+                if (ctx.uid1 != null) {
+                    comboId = ctx.uid1.getText();
                 }
-                return ctx.getText();
+
+                Aggregator agg = new Aggregator();
+                double aggregatedValue = agg.aggregateValuesDataElements(ctx.uid0.getText(), comboId, period, orgunit);
+
+                return aggregatedValue;
 
             } else if (ctx.numericLiteral() != null) {
 
-                double x = BigDecimal.valueOf(castDouble(ctx.numericLiteral().getText()))
+                double val = BigDecimal.valueOf(castDouble(ctx.numericLiteral().getText()))
                         .doubleValue();
-                return x;
+                return val;
             }
+
+        } else {
+            if (ctx.numericLiteral() != null) {
+                double val = BigDecimal.valueOf(castDouble(ctx.numericLiteral().getText()))
+                        .doubleValue();
+                return val;
+            };
         }
         return 0;
     }
