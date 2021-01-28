2f5c688d0867ae749c38bb9cd899ab4971a77bd6
Duncan Ndiithi <duncanndiithi@yahoo.com>
2021-01-28 10:13:15 +0300

    String utils

---
+++ src/main/java/com/healthit/indicator_calculator/util/Stringzz.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.healthit.indicator_calculator.util;
+
+import java.util.List;
+
+/**
+ *
+ * @author duncanndiithi
+ */
+public class Stringzz {
+    public static String buildCommaSeperatedString(List<String> values){
+        StringBuilder commaSeperatedValues = new StringBuilder();
+        boolean added = false;
+        for (String value : values) {
+            if (added) {
+                commaSeperatedValues.append("," + "'" + value + "'");
+            } else {
+                commaSeperatedValues.append(value);
+                added = true;
+            }
+        }
+        return commaSeperatedValues.toString();
+    }
+}
